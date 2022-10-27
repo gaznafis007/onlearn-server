20158e69f10c216af5d34ad12a6b6dab24a7f78f
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2022-10-27 13:06:40 +0600

    basic set up

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const datas = require("./data/datas.json");
+
 app.get("/", (req, res) => {
   res.send("API is running");
+});
+
+app.get("/datas", (req, res) => {
+  res.send(datas);
+});
+
+app.get("/datas/:category", (req, res) => {
+  const category = req.params.category;
+  const selectedCategory = datas.filter((data) => data.category === category);
+  res.send(selectedCategory);
 });
 
 app.listen(port, () => {
